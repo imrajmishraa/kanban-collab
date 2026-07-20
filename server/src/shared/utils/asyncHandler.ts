b92bb8e6256b39b2521e43c2c4430528c92c312e
Raj Mishra <raj212558@gmail.com>
2026-07-20 19:56:24 +0530

    feat: add async route handler utility

---
+++ server/src/shared/utils/asyncHandler.ts
@@ -0,0 +1,14 @@
+import { type Request, type Response, type NextFunction  } from "express";
+
+type AsyncHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<unknown>;
+
+const asyncHandler = (handler: AsyncHandler) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(handler(req, res, next).catch(next));
+  }
+
+export { asyncHandler };
