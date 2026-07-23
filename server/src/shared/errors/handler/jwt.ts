1627ab1b0e0c9462102e63ca63ce07aca0edca19
Raj Mishra <raj212558@gmail.com>
2026-07-23 10:53:01 +0530

    feat: add JWT error normalization

--- server/src/shared/errors/handler/jwt.ts
+++ server/src/shared/errors/handler/jwt.ts
@@ -1,13 +1,19 @@
 import jwt from "jsonwebtoken";
 import { ApiError } from "../../../shared/utils/ApiError";
+import { invalidAccessTokenError, expiredAccessTokenError } from "../auth/accessToken";
+
 
 export const handleJwtError = (err: unknown): ApiError | null => {
   if (err instanceof jwt.TokenExpiredError) {
-    return new ApiError(401, "Access token expired");
+    return expiredAccessTokenError();
   }
 
   if (err instanceof jwt.JsonWebTokenError) {
-    return new ApiError(401, "Invalid access token");
+    return invalidAccessTokenError();
+  }
+
+  if (err instanceof jwt.NotBeforeError ) {
+    return new ApiError(401, "Token is not active yet.")
   }
 
   return null;
