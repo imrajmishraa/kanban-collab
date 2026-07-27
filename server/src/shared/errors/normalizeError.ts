ec15755ae1e730e6d2cfec1848e9b4b5b7d96f4e
Raj Mishra <raj212558@gmail.com>
2026-07-27 23:27:53 +0530

    refactor(errors): improve error normalization pipeline

--- server/src/shared/errors/normalizeError.ts
+++ server/src/shared/errors/normalizeError.ts
@@ -1,20 +1,30 @@
 import { ApiError } from "../utils/ApiError";
+
+import { internalServerError } from "./handler/custom";
+import { handleJwtError } from "./handler/jwt";
 import { handleMongooseError } from "./handler/mongoose";
 import { handleRedisError } from "./handler/redis";
 import { handleZodError } from "./handler/zod";
-import { handleJwtError } from "./handler/jwt";
-import { internalServerError } from "./handler/custom";
 
-export const normalizeError = (err: unknown): ApiError => {
+const handlers = Object.freeze([
+  handleMongooseError,
+  handleRedisError,
+  handleZodError,
+  handleJwtError,
+]);
+
+export function normalizeError(err: unknown): ApiError {
   if (err instanceof ApiError) {
     return err;
   }
 
-  return (
-    handleMongooseError(err) ??
-    handleRedisError(err) ??
-    handleZodError(err) ??
-    handleJwtError(err) ??
-    internalServerError()
-  );
-};
+  for (const handler of handlers) {
+    const normalized = handler(err);
+
+    if (normalized) {
+      return normalized;
+    }
+  }
+
+  return internalServerError();
+}
