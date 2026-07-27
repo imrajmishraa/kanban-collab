c9da05ceea696a543eb7067ae28de5caa9f52f6d
Raj Mishra <raj212558@gmail.com>
2026-07-28 01:16:54 +0530

    refactor(websocket): improve authorization middleware

--- server/src/interfaces/websockets/middlewares/authorize.ts
+++ server/src/interfaces/websockets/middlewares/authorize.ts
@@ -5,22 +5,36 @@ import {
   WorkspaceModel,
 } from "../../../infrastructure/db/mongoose/schemas";
 
+import { boardNotFoundError } from "../../../shared/errors/board/board";
+
+import { notWorkspaceMemberError } from "../../../shared/errors/workspace/workspace";
+
+import { invalidObjectIdError } from "../../../shared/errors/handler/custom";
+
 export async function authorize(
   userId: string,
   boardId: string,
 ): Promise<void> {
-  const board = await BoardModel.findById(boardId);
+  if (!Types.ObjectId.isValid(userId) || !Types.ObjectId.isValid(boardId)) {
+    throw invalidObjectIdError();
+  }
+
+  const board = await BoardModel.findById(boardId)
+    .select("_id workspaceId")
+    .lean();
 
   if (!board) {
-    throw new Error("Board not found.");
+    throw boardNotFoundError();
   }
 
   const workspace = await WorkspaceModel.findOne({
     _id: board.workspaceId,
     "members.userId": new Types.ObjectId(userId),
-  });
+  })
+    .select("_id")
+    .lean();
 
   if (!workspace) {
-    throw new Error("Forbidden.");
+    throw notWorkspaceMemberError();
   }
 }
