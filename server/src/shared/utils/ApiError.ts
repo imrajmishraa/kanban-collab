483acca4ba3d7e6f2f6eae7abb53ce33848affb4
Raj Mishra <raj212558@gmail.com>
2026-07-27 23:56:16 +0530

    refactor(errors): improve ApiError implementation

--- server/src/shared/utils/ApiError.ts
+++ server/src/shared/utils/ApiError.ts
@@ -1,9 +1,9 @@
 class ApiError extends Error {
   public readonly statusCode: number;
-  public readonly success = false;
+  public readonly success: boolean = false;
   public readonly errors: unknown[];
   public readonly data: unknown;
-  public readonly isOperational = true;
+  public readonly isOperational: boolean = true;
 
   constructor(
     statusCode: number,
@@ -13,6 +13,10 @@ class ApiError extends Error {
     stack?: string,
   ) {
     super(message);
+
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    this.name = "ApiError";
 
     this.statusCode = statusCode;
     this.errors = errors;
