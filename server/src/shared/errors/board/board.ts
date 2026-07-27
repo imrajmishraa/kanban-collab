f561548256fe6b091bfe9b0cbfac2e0aca224edd
Raj Mishra <raj212558@gmail.com>
2026-07-27 23:55:44 +0530

    refactor(errors): improve board error definitions

--- server/src/shared/errors/board/board.ts
+++ server/src/shared/errors/board/board.ts
@@ -1,13 +1,25 @@
+import { ERROR_MESSAGE } from "../../constants/error";
+import { HTTP_STATUS } from "../../constants/http";
 import { ApiError } from "../../utils/ApiError";
 
-export const guestNotCreateBoards = () =>
-  new ApiError(401, "Forbidden: Guests cannot make changes");
+export function guestCannotModifyBoardError(): ApiError {
+  return new ApiError(
+    HTTP_STATUS.FORBIDDEN,
+    ERROR_MESSAGE.GUEST_CANNOT_MODIFY_BOARD,
+  );
+}
 
+export function boardNotFoundError(): ApiError {
+  return new ApiError(HTTP_STATUS.NOT_FOUND, ERROR_MESSAGE.BOARD_NOT_FOUND);
+}
 
-export const boardNotFoundError = () => 
-    new ApiError(404, 'Board not found');
+export function boardAccessDeniedError(): ApiError {
+  return new ApiError(HTTP_STATUS.FORBIDDEN, ERROR_MESSAGE.BOARD_ACCESS_DENIED);
+}
 
-export const boardWorkspaceAccessDeniedError = () => new ApiError(401, "Forbidden: Board workspace access denied");
-
-export const boardIdAndQueryParametersRequiredError = () =>
-  new ApiError(400, "boardId and query parameters are required");
+export function boardIdAndQueryParametersRequiredError(): ApiError {
+  return new ApiError(
+    HTTP_STATUS.BAD_REQUEST,
+    ERROR_MESSAGE.BOARD_ID_AND_QUERY_REQUIRED,
+  );
+}
