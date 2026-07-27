6cf7e0aa91ce787638524c7071b822031fb960f3
Raj Mishra <raj212558@gmail.com>
2026-07-27 23:55:50 +0530

    refactor(errors): improve card error definitions

--- server/src/shared/errors/card/card.ts
+++ server/src/shared/errors/card/card.ts
@@ -1,3 +1,7 @@
+import { ERROR_MESSAGE } from "../../constants/error";
+import { HTTP_STATUS } from "../../constants/http";
 import { ApiError } from "../../utils/ApiError";
 
-export const cardNotFoundError = () => new ApiError(404, "Card not found");
+export function cardNotFoundError(): ApiError {
+  return new ApiError(HTTP_STATUS.NOT_FOUND, ERROR_MESSAGE.CARD_NOT_FOUND);
+}
