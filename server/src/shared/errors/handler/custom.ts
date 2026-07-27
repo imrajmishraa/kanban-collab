b117aaba2a6a162c7fb99da0df792393c960ff55
Raj Mishra <raj212558@gmail.com>
2026-07-27 23:27:03 +0530

    refactor(errors): improve custom error handler

--- server/src/shared/errors/handler/custom.ts
+++ server/src/shared/errors/handler/custom.ts
@@ -1,5 +1,10 @@
+import { HTTP_STATUS } from "../../constants/http";
+import { ERROR_MESSAGE } from "../../constants/error";
 import { ApiError } from "../../utils/ApiError";
 
-export const internalServerError = () =>
-  new ApiError(500, "Internal Server Error");
-
+export function internalServerError(): ApiError {
+  return new ApiError(
+    HTTP_STATUS.INTERNAL_SERVER_ERROR,
+    ERROR_MESSAGE.INTERNAL_SERVER_ERROR,
+  );
+}
