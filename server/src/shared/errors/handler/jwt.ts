6d1271222433337f677fcee81d9b831a7d8f5f8d
Raj Mishra <raj212558@gmail.com>
2026-07-27 23:27:16 +0530

    refactor(errors): improve JWT error handling

--- server/src/shared/errors/handler/jwt.ts
+++ server/src/shared/errors/handler/jwt.ts
@@ -1,20 +1,30 @@
 import jwt from "jsonwebtoken";
-import { ApiError } from "../../../shared/utils/ApiError";
-import { invalidAccessTokenError, expiredAccessTokenError } from "../auth/accessToken";
 
+import { ERROR_MESSAGE } from "../../constants/error";
+import { HTTP_STATUS } from "../../constants/http";
+import { ApiError } from "../../utils/ApiError";
 
-export const handleJwtError = (err: unknown): ApiError | null => {
+export function handleJwtError(err: unknown): ApiError | null {
   if (err instanceof jwt.TokenExpiredError) {
-    return expiredAccessTokenError();
+    return new ApiError(
+      HTTP_STATUS.UNAUTHORIZED,
+      ERROR_MESSAGE.ACCESS_TOKEN_EXPIRED,
+    );
   }
 
   if (err instanceof jwt.JsonWebTokenError) {
-    return invalidAccessTokenError();
+    return new ApiError(
+      HTTP_STATUS.UNAUTHORIZED,
+      ERROR_MESSAGE.INVALID_ACCESS_TOKEN,
+    );
   }
 
-  if (err instanceof jwt.NotBeforeError ) {
-    return new ApiError(401, "Token is not active yet.")
+  if (err instanceof jwt.NotBeforeError) {
+    return new ApiError(
+      HTTP_STATUS.UNAUTHORIZED,
+      ERROR_MESSAGE.ACCESS_TOKEN_NOT_ACTIVE,
+    );
   }
 
   return null;
-};
+}
