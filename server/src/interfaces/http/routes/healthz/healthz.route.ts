95d64d834cb2222718cce6ca5e34a4cd77b9994c
Raj Mishra <raj212558@gmail.com>
2026-08-04 11:28:23 +0530

    feat(healthz): add websocket health check route

--- server/src/interfaces/http/routes/healthz/healthz.route.ts
+++ server/src/interfaces/http/routes/healthz/healthz.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { healthz } from "../../controllers/healthz/healthz"
+import { healthz } from "../../controllers/healthz/healthz";
+import { websocketHealth } from "../../controllers/healthz/websocketHealth";
 
 const router = Router();
 
 router.get("/healthz", healthz);
+router.get("/healthz/websocket", websocketHealth);
 
 export default router;
