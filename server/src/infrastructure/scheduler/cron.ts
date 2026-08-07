6f651d267a67ba037fba3e54de02cb19b42414a7
Raj Mishra <raj212558@gmail.com>
2026-08-07 22:39:45 +0530

    feat(scheduler): add cron job scheduler

---
+++ server/src/infrastructure/scheduler/cron.ts
@@ -0,0 +1,19 @@
+import cron from "node-cron";
+import { schedulerLogger } from "../logging/childLogger";
+import { workspaceDeletionJob } from "../../jobs/workspaceDeletion.job";
+
+
+export const startCronJobs = (): void => {
+  cron.schedule("0 * * * *", async () => {
+    try {
+      await workspaceDeletionJob();
+    } catch (error) {
+      schedulerLogger.error(
+        { err: error },
+        "Workspace deletion cron job failed",
+      );
+    }
+  });
+
+  schedulerLogger.info("Cron jobs started successfully");
+};
