ed71a3d22f80948fc472db1d9d874aa7d38692be
Raj Mishra <raj212558@gmail.com>
2026-08-08 23:53:07 +0530

    feat(websocket): improve websocket errors

--- server/src/shared/errors/websocket/websocket.ts
+++ server/src/shared/errors/websocket/websocket.ts
@@ -2,10 +2,19 @@ import { ERROR_MESSAGE } from "../../constants/error";
 import { HTTP_STATUS } from "../../constants/http";
 import { ApiError } from "../../utils/ApiError";
 
-
 export function InvalidWebSocketRequestError(): ApiError {
   return new ApiError(
     HTTP_STATUS.BAD_REQUEST,
     ERROR_MESSAGE.ACCESS_TOKEN_INVALID,
   );
+}
+
+export class InvalidCollaborationMessageError extends Error {
+  public readonly code = "INVALID_COLLABORATION_MESSAGE";
+
+  constructor(message = "Invalid collaboration message.") {
+    super(message);
+
+    this.name = "InvalidCollaborationMessageError";
+  }
 }
