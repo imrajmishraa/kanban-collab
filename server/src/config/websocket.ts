00e1648da9265f687a0e83daaa8b30323d601cbf
Raj Mishra <raj212558@gmail.com>
2026-08-09 20:54:03 +0530

    refactor(websocket): update websocket configuration

--- server/src/config/websocket.ts
+++ server/src/config/websocket.ts
@@ -3,6 +3,10 @@ import { ENV } from "./env";
 export const websocketConfig = Object.freeze({
   persistenceDebounceMs: 5_000,
 
+  idleCleanupMs: 5 * 60 * 1_000,
+
+  shutdownTimeoutMs: 10_000,
+
   redisChannelPrefix: "room:",
 
   heartbeat: {
