211ad0687fc7b04f3c2cdae6fc41597a2b7ca041
Raj Mishra <raj212558@gmail.com>
2026-08-09 20:55:33 +0530

    refactor(yjs): update collaboration protocol types

--- server/src/interfaces/websockets/collaboration/Yjs/types.ts
+++ server/src/interfaces/websockets/collaboration/Yjs/types.ts
@@ -1,9 +1,10 @@
 import type { WebSocket } from "ws";
-import * as Y from 'yjs';
-
+import * as Y from "yjs";
+import type { Awareness } from "y-protocols/awareness";
 
 export interface CollaborationClient {
   id: string;
+  userId: string;
   socket: WebSocket;
 }
 
@@ -15,7 +16,6 @@ export interface DecodedMessage {
 export interface AwarenessState {
   userId: string;
   name?: string;
-
   cursor?: {
     cardId?: string;
   };
@@ -24,10 +24,16 @@ export interface AwarenessState {
 export interface ManagedDocument {
   readonly name: string;
   readonly doc: Y.Doc;
+  readonly awareness: Awareness;
+
   readonly createdAt: Date;
+
   updatedAt: Date;
   lastAccessedAt: Date;
+
   connectionCount: number;
   loaded: boolean;
   destroyed: boolean;
+
+  clients: Map<string, CollaborationClient>;
 }
