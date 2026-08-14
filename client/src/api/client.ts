1c692934ba580aad7ec0b7a766668a3ac1d76b10
Raj Mishra <raj212558@gmail.com>
2026-08-14 22:49:02 +0530

    refactor(client): update api client

---
+++ client/src/api/client.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import { getAccessToken } from "@/stores/useAuthStore";
+
+export const api = axios.create({
+  baseURL: "/api/v1",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const accessToken = getAccessToken();
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
