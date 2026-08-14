b1220af5bad02f443937390d93b65088a5274e8a
Raj Mishra <raj212558@gmail.com>
2026-08-14 22:49:46 +0530

    refactor(client): update application entrypoint

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app/App.tsx'
+import App from './app/App';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+if (!container) throw new Error('Missing #root element')
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>,
