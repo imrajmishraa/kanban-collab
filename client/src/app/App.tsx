a402768c02afa04180828fa6cfa1bfcfdf7e2fd3
Raj Mishra <raj212558@gmail.com>
2026-08-16 23:13:34 +0530

    refactor(app): update application root

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,15 +2,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from "@app/router";
 import { AuthProvider } from './providers/AuthProvider';
 import { ThemeProvider } from './providers/ThemeProvider';
+import QueryProvider from "./providers/QueryProvider";
+
+import ScrollToTop from './ScrollToTop';
 
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <ThemeProvider>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
+        <QueryProvider>
+          <AuthProvider>
+            <AppRouter />
+          </AuthProvider>
+        </QueryProvider>
       </ThemeProvider>
     </BrowserRouter>
   );
